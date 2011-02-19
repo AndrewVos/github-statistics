40dd5d9d9722a02863f3e832a7a7001322390f97
AndrewVos <andrew.vos@gmail.com>
2011-02-19 14:39:24 +0000

    Refactored the rip_url method into get_repository

--- spec/github_data_ripper.rb
+++ spec/github_data_ripper.rb
@@ -8,13 +8,13 @@ class GitHubDataRipper
     def rip_data
       ['C#', 'C++', 'Python', 'Ruby'].each do |language|
         (1..30).each do |page|
-          rip_url %{http://github.com/api/v2/json/repos/search/+?type=Repositories&language=#{language}&start_page=#{page}}
+          get_repositories(language, page)
         end
       end
     end
 
-    def rip_url(url)
-      uri = URI.parse(url)
+    def get_repositories(language, page)
+      uri = URI.parse %{http://github.com/api/v2/json/repos/search/+?type=Repositories&language=#{language}&start_page=#{page}}
       json = nil
       while json == nil
         json = Net::HTTP.get(uri)
@@ -35,60 +35,53 @@ describe GitHubDataRipper do
   describe ".rip_data" do
 
     it "rips all languages" do
-      expected_urls = []
+      expected_messages = []
       ['C#', 'C++', 'Python', 'Ruby'].each do |language|
         (1..30).each do |page|
-          expected_urls.push %{http://github.com/api/v2/json/repos/search/+?type=Repositories&language=#{language}&start_page=#{page}}
+          expected_messages.push([language, page])
         end
       end
-      GitHubDataRipper.should_receive(:rip_url).exactly(120).times do |url|
-        expected_urls.delete(url)
+      GitHubDataRipper.should_receive(:get_repositories).exactly(120).times do |language, page|
+        expected_messages.delete([language, page])
       end
       GitHubDataRipper.rip_data
-      expected_urls.size.should == 0
+      expected_messages.size.should == 0
     end
 
   end
 
-  describe ".rip_url" do
+  describe ".get_repositories" do
 
     before :each do
-      @url = "http://www.example.org"
+      @url = %{http://github.com/api/v2/json/repos/search/+?type=Repositories&language=ruby&start_page=1}
       @uri = URI.parse(@url)
-    end
-
-    it "should parse the url" do
       URI.should_receive(:parse).once.with(@url).and_return(@uri)
-      GitHubDataRipper.rip_url(@url)
     end
 
     it "should get the url" do
-      URI.should_receive(:parse).once.with(@url).and_return(@uri)
       Net::HTTP.should_receive(:get).once.with(@uri).and_return("json")
-      GitHubDataRipper.rip_url(@url)
+      GitHubDataRipper.get_repositories("ruby", 1)
     end
 
     it "should return the json" do
-      URI.should_receive(:parse).once.with(@url).and_return(@uri)
       Net::HTTP.should_receive(:get).once.with(@uri).and_return("my json stuff")
-      GitHubDataRipper.rip_url(@url).should == "my json stuff"
+      GitHubDataRipper.get_repositories("ruby", 1).should == "my json stuff"
     end
 
     context "with one rate limit exceeded response and one valid response" do
 
       before :each do
-        URI.should_receive(:parse).once.with(@url).and_return(@uri)
         Net::HTTP.stub!(:get).and_return(%{{"error":["Rate Limit Exceeded for 127.0.0.1"]}}, "some json")
       end
 
       it "tries again if it gets a rate limit exceeded response" do
         Net::HTTP.should_receive(:get).with(@uri).twice
-        GitHubDataRipper.rip_url(@url)
+        GitHubDataRipper.get_repositories("ruby", 1)
       end
 
       it "sleeps for a second if it gets a rate limit exceeded response" do
         GitHubDataRipper.should_receive(:sleep).with(1).once
-        GitHubDataRipper.rip_url(@url)
+        GitHubDataRipper.get_repositories("ruby", 1)
       end
 
     end
@@ -96,14 +89,13 @@ describe GitHubDataRipper do
     context "with two rate limit exceeded responses and one valid response" do
 
       before :each do
-        URI.should_receive(:parse).once.with(@url).and_return(@uri)
         rate_limit_exceeded = %{{"error":["Rate Limit Exceeded for 127.0.0.1"]}}
         Net::HTTP.stub!(:get).and_return(rate_limit_exceeded, rate_limit_exceeded, "valid json")
       end
 
       it "tries multiple times if it gets a rate limit exceeded response" do
         Net::HTTP.should_receive(:get).with(@uri).exactly(3).times
-        GitHubDataRipper.rip_url(@url)
+        GitHubDataRipper.get_repositories("ruby", 1)
       end
 
     end
