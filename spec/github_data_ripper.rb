806d5822d4b7c25c05293fd580ffb82a082aa910
AndrewVos <andrew.vos@gmail.com>
2011-02-19 15:36:41 +0000

    cleaned up some tests

--- spec/github_data_ripper.rb
+++ spec/github_data_ripper.rb
@@ -4,6 +4,22 @@ describe GitHubDataRipper do
 
   describe ".rip_data" do
 
+    before :each do
+      @json = <<-JSON
+        {"repositories":[
+          {"type":"repo","open_issues":1,"forks":3,"language":"Ruby","url":"https://github.com/rsim/ruby-plsql","has_issues":true,"homepage":"","has_downloads":true,"pushed":"2010/12/10 04:05:40 -0800","fork":false,"pushed_at":"2010/12/10 04:05:40 -0800","followers":38,"created_at":"2008/04/19 07:49:12 -0700","score":0.3126592,"size":296,"private":false,"created":"2008/04/19 07:49:12 -0700","name":"ruby-plsql","owner":"rsim","has_wiki":true,"watchers":38,"username":"rsim","description":"ruby-plsql gem provides simple Ruby API for calling Oracle PL/SQL procedures. It could be used both for accessing Oracle PL/SQL API procedures in legacy applications as well as it could be used to create PL/SQL unit tests using Ruby testing libraries."}
+        ]}
+      JSON
+
+      @repository = [
+        {:user_id => "rsim", :repository => "ruby-plsql"}
+      ]
+
+      @all_repositories = []
+      (1..120).each { @all_repositories << @repository }
+      @all_repositories.flatten!
+    end
+
     it "rips all languages" do
       expected_messages = []
       ['C#', 'C++', 'Python', 'Ruby'].each do |language|
@@ -19,23 +35,8 @@ describe GitHubDataRipper do
     end
 
     it "converts the data to yaml" do
-      json = <<-JSON
-        {"repositories":[
-          {"type":"repo","open_issues":1,"forks":3,"language":"Ruby","url":"https://github.com/rsim/ruby-plsql","has_issues":true,"homepage":"","has_downloads":true,"pushed":"2010/12/10 04:05:40 -0800","fork":false,"pushed_at":"2010/12/10 04:05:40 -0800","followers":38,"created_at":"2008/04/19 07:49:12 -0700","score":0.3126592,"size":296,"private":false,"created":"2008/04/19 07:49:12 -0700","name":"ruby-plsql","owner":"rsim","has_wiki":true,"watchers":38,"username":"rsim","description":"ruby-plsql gem provides simple Ruby API for calling Oracle PL/SQL procedures. It could be used both for accessing Oracle PL/SQL API procedures in legacy applications as well as it could be used to create PL/SQL unit tests using Ruby testing libraries."}
-        ]}
-      JSON
-
-      data = [
-        {:user_id => "rsim", :repository => "ruby-plsql"}
-      ]
-
-      GitHubDataRipper.should_receive(:get_repositories).exactly(120).times.and_return(data)
-
-      all_data = []
-      (1..120).each { all_data << data }
-      all_data.flatten!
-      YAML.should_receive(:dump).with(all_data)
-
+      GitHubDataRipper.should_receive(:get_repositories).exactly(120).times.and_return(@repository)
+      YAML.should_receive(:dump).with(@all_repositories)
       GitHubDataRipper.rip_data
     end
   end
